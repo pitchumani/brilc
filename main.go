3836e26b84a49e240ce035decf1fc38f9af72087
Pitchumani Sivanupandi <s.pitchumani@gmail.com>
2021-02-03 13:55:32 +0530

    Parse functions and instructions.

--- main.go
+++ main.go
@@ -7,6 +7,23 @@ import (
     "encoding/json"
 )
 
+func compile_function (function map[string]interface{}) {
+    fmt.Println ("Function: ", function["name"].(string))
+    // ignore args for now
+    // process instructions (array of json objects)
+    instrs := function["instrs"].([]interface{})
+    insnCount := len(instrs)
+    fmt.Println ("Total insns: ", insnCount)
+    for insnIndex := 0; insnIndex < insnCount; insnIndex++ {
+        fmt.Println ("insn ", insnIndex, ":")
+        // each instruction is a set of json objects (can map with key, value)
+        insn := instrs[insnIndex].(map[string]interface{})
+        for ikey, ival := range insn {
+            fmt.Println ("  attribs ", ikey, ":", ival)
+        }
+    }
+}
+
 func compile_file (fname string) {
     fmt.Println ("Compiling file: ", fname)
     jfile, err := os.Open (fname)
@@ -20,6 +37,9 @@ func compile_file (fname string) {
     json.Unmarshal(byteValue, &program)
     functions := program["functions"].([]interface{})
     fmt.Println("nfuncs: ", len(functions))
+    for i := 0; i < len(functions); i++ {
+        compile_function (functions[i].(map[string]interface{}))
+    }
 }
 
 func main () {
